df02e5938020bed8551424c9127d0ea78ff23550
ZacHooper <zac.g.hooper@gmail.com>
2021-04-27 21:29:20 +1000

    Added Ship, User, Location & Loan models

---
+++ SpacePyTraders/models.py
@@ -0,0 +1,179 @@
+class User ():
+    """The Ship is awesome!"""
+    def __init__(self, data=None, username=None, credits=None, ships=None, loans=None):
+        # Regular Init
+        self.username = username 
+        self.credits = credits
+        self.ships = [Ship(ship) for ship in ships['ships']] if isinstance(ships, dict) else ships
+        self.loans = [Loan(loan) for loan in loan['loans']] if isinstance(loans, dict) else loans
+        # Init with JSON
+        if data is not None and isinstance(data, dict):
+            if 'user' in data:
+                data = data['user']
+            self.username = data['username']
+            self.credits = data['credits']
+            self.ships = [Ship(ship) for ship in data['ships']]
+            self.loans = [Loan(loan) for loan in data['loans']]
+        # Init with User object
+        if data is not None and isinstance(data, User):
+            self.username = data.username
+            self.credits = data.credits
+            self.ships = data.ships
+            self.loans = data.loans
+        
+    def asDict(self):
+        return_dict = {'username': self.username, 'credits': self.credits}
+        return_dict['ships'] = [ship.asDict() for ship in self.ships]
+        return_dict['loans'] = [loan.asDict() for loan in self.loans]
+        return return_dict
+    
+    def __repr__(self):
+        return f"Username: {self.username}, Credits: {self.credits}, No of Ships: {len(self.ships)}, No of Loans: {len(self.loans)}"
+    
+    def __str__(self):
+        return f"Username: {self.username}, Credits: {self.credits}, No of Ships: {len(self.ships)}, No of Loans: {len(self.loans)}"
+
+
+class Ship ():
+    """The Ships is Awesome!"""
+    def __init__(self, data=None, id=None, manufacturer=None, kind=None, type=None, location=None, x=None, y=None, flightPlanId=None, speed=None, plating=None, weapons=None, maxCargo=None, spaceAvailable=None, cargo=None, **kwargs):
+        """The ship class
+        """
+        # Regular init
+        self.id = id
+        self.manufacturer = manufacturer
+        self.kind = kind
+        self.type = type
+        self.location = location
+        self.x = x
+        self.y = y
+        self.flightPlanId = flightPlanId
+        self.speed = speed
+        self.plating = plating
+        self.weapons = weapons
+        self.maxCargo = maxCargo
+        self.spaceAvailable = spaceAvailable
+        self.cargo = cargo
+        # If data is JSON
+        if data is not None and isinstance(data, dict):
+            if 'ship' in data:
+                data = data['ship']
+            if 'location' not in data:
+                self.location = "IN TRANSIT"
+                self.x = None
+                self.y = None
+            if 'flightPlanId' not in data:
+                self.flightPlanId = None
+            self.id = data['id']
+            self.manufacturer = data['manufacturer']
+            self.kind = data['class']
+            self.type = data['type']
+            self.speed = data['speed']
+            self.plating = data['plating']
+            self.weapons = data['weapons']
+            self.maxCargo = data['maxCargo']
+            self.spaceAvailable = data['spaceAvailable']
+            self.cargo = data['cargo']
+        # If data is a Ship object
+        if data is not None and isinstance(data, Ship):
+            self.id = data.id
+            self.manufacturer = data.manufacturer
+            self.kind = data.kind
+            self.type = data.type
+            self.location = data.location
+            self.x = data.x
+            self.y = data.y
+            self.flightPlanId = data.flightPlanId
+            self.speed = data.speed
+            self.plating = data.plating
+            self.weapons = data.weapons
+            self.maxCargo = data.maxCargo
+            self.spaceAvailable = data.spaceAvailable
+            self.cargo = data.cargo
+
+    def asDict(self):
+        return self.__dict__
+                
+    def __repr__(self):
+        return f"ID: {self.id}, Manufacturer: {self.manufacturer}, Class: {self.kind} " \
+               f"Type: {self.type}, Location: {self.location}, X: {self.x} " \
+               f"Y: {self.y}, Flight Plan ID: {self.flightPlanId}, Speed: {self.speed} " \
+               f"Plating: {self.plating}, Weapons: {self.weapons}, Max Cargo: {self.maxCargo} " \
+               f"Space Available: {self.spaceAvailable}, No of Goods in Cargo: {len(self.cargo)}"
+    
+    def __str__(self):
+        return f"ID: {self.id}, Manufacturer: {self.manufacturer}, Class: {self.kind} " \
+               f"Type: {self.type}, Location: {self.location}, X: {self.x} " \
+               f"Y: {self.y}, Flight Plan ID: {self.flightPlanId}, Speed: {self.speed} " \
+               f"Plating: {self.plating}, Weapons: {self.weapons}, Max Cargo: {self.maxCargo} " \
+               f"Space Available: {self.spaceAvailable}, No of Goods in Cargo: {len(self.cargo)}"
+
+class Loan ():
+    """The Ship is awesome!"""
+    def __init__(self, data=None, id=None, due=None, repaymentAmount=None, status=None, type=None):
+        # Regular Init
+        self.id = id 
+        self.due = due
+        self.repaymentAmount = repaymentAmount
+        self.status = status
+        self.type = type
+        # Init with JSON
+        if data is not None and isinstance(data, dict):
+            if 'user' in data:
+                data = data['user']
+            self.id = data['id']
+            self.due = data['due']
+            self.repaymentAmount = data['repaymentAmount']
+            self.status = data['status']
+            self.type = data['type']
+        # Init with User object
+        if data is not None and isinstance(data, User):
+            self.id = data.id
+            self.due = data.due
+            self.repaymentAmount = data.repaymentAmount
+            self.status = data.status
+            self.type = data.type
+        
+    def asDict(self):
+        return {'id': self.id, 'due': self.due, 'repaymentAmount': self.repaymentAmount, 'status': self.status, 'type': self.type}
+    
+    def __repr__(self):
+        return f"id: {self.id}, due: {self.due}, repaymentAmount: {self.repaymentAmount}, status: {self.status}, type: {self.type}"
+    
+    def __str__(self):
+        return f"id: {self.id}, due: {self.due}, repaymentAmount: {self.repaymentAmount}, status: {self.status}, type: {self.type}"
+
+class Location ():
+    """The Ship is awesome!"""
+    def __init__(self, data=None, symbol=None, type=None, name=None, x=None, y=None):
+        # Regular Init
+        self.symbol = symbol 
+        self.y = y
+        self.name = name
+        self.x = x
+        self.y = y
+        # Init with JSON
+        if data is not None and isinstance(data, dict):
+            if 'user' in data:
+                data = data['user']
+            self.symbol = data['symbol']
+            self.y = data['y']
+            self.name = data['name']
+            self.x = data['x']
+            self.y = data['y']
+        # Init with User object
+        if data is not None and isinstance(data, User):
+            self.symbol = data.symbol
+            self.y = data.y
+            self.name = data.name
+            self.x = data.x
+            self.y = data.y
+        
+    def asDict(self):
+        return {'symbol': self.symbol, 'type': self.type, 'name': self.name, 'x': self.x, 'y': self.y}
+    
+    def __repr__(self):
+        return f"symbol: {self.symbol}, type: {self.type}, name: {self.name}, x: {self.x}, y: {self.y}"
+    
+    def __str__(self):
+        return f"symbol: {self.symbol}, type: {self.type}, name: {self.name}, x: {self.x}, y: {self.y}"
---
+++ Tests/test_models.py
@@ -0,0 +1,95 @@
+import unittest
+import logging
+import json
+from SpacePyTraders.models import *
+
+TOKEN = "0930cc36-7dc7-4cb1-8823-d8e72594d91e"
+USERNAME = "JimHawkins"
+
+with open('./Tests/model_mocks.json', 'r') as infile:
+    MOCKS = json.load(infile)
+
+class TestUserInit(unittest.TestCase):
+    def test_user_init_manual(self):
+        self.assertIsInstance(User(username="JimHawkins", credits=0, ships=[], loans=[]), User, "User model did not initiate properly")
+
+    def test_user_init_json(self):
+        self.assertIsInstance(User(MOCKS['user']), User, "User model did not initiate properly")
+
+    def test_user_init_user(self):
+        self.assertIsInstance(User(User(MOCKS['user'])), User, "User model did not initiate properly")
+
+class TestUserMethods(unittest.TestCase):
+    def setUp(self):
+        self.user = User(data=MOCKS['user'])
+
+    def test_ships_are_ship_objects(self):
+        self.assertTrue(all(isinstance(ship, Ship) for ship in self.user.ships), "Not all of the ships are a Ship Object")
+
+    def test_loans_are_loan_objects(self):
+        self.assertTrue(all(isinstance(loan, Loan) for loan in self.user.loans), "Not all of the loans are a Loan Object")
+
+    def test_user_property_updates(self):
+        self.user.username = "Zac"
+        self.assertEqual(self.user.username, "Zac", "Username did not correctly update")
+
+class TestShipInit(unittest.TestCase):
+    def test_ship_init_manual(self):
+        self.assertIsInstance(Ship(id="213456", manufacturer="Gravader", kind="MK-I", 
+                                   type="GR-MK-I", location="OE-PM", x=-28, y=25, 
+                                   flightPlanId=None, speed=1, plating=10, 
+                                   weapons=5, maxCargo=100, spaceAvailable=99, 
+                                   cargo=[]), Ship, "User model did not initiate properly")
+
+    def test_ship_init_json(self):
+        self.assertIsInstance(Ship(MOCKS['ship']), Ship, "User model did not initiate properly")
+
+    def test_ship_init_ship(self):
+        self.assertIsInstance(Ship(Ship(MOCKS['ship'])), Ship, "User model did not initiate properly")
+
+class TestShipMethods(unittest.TestCase):
+    def setUp(self):
+        self.ship = Ship(MOCKS['ship'])
+
+    def test_ship_updates(self):
+        self.ship.id = "Zac"
+        self.assertEqual(self.ship.id, "Zac", "ID did not correctly update") 
+
+class TestLoanInit(unittest.TestCase):
+    def test_loan_init_manual(self):
+        self.assertIsInstance(Loan(id="213456", due="2021-04-27T23:12:27.516Z", repaymentAmount=280000, 
+                                    type="STARTUP"), Loan, "Loan model did not initiate properly")
+
+    def test_loan_init_json(self):
+        self.assertIsInstance(Loan(MOCKS['user_loan']), Loan, "Loan model did not initiate properly")
+
+    def test_loan_init_loan(self):
+        self.assertIsInstance(Loan(Loan(MOCKS['user_loan'])), Loan, "Loan model did not initiate properly")
+
+class TestLoanMethods(unittest.TestCase):
+    def setUp(self):
+        self.loan = Loan(MOCKS['user_loan'])
+
+    def test_loan_updates(self):
+        self.loan.id = "Zac"
+        self.assertEqual(self.loan.id, "Zac", "ID did not correctly update")
+
+class TestLocationInit(unittest.TestCase):
+    def test_location_init_manual(self):
+        self.assertIsInstance(Location(symbol="OE-PM", type="PLANET", name="Prime", x=4, y=12), 
+                              Location, "Location model did not initiate properly")
+
+    def test_location_init_json(self):
+        self.assertIsInstance(Location(MOCKS['location']), Location, "Location model did not initiate properly")
+
+    def test_location_init_location(self):
+        self.assertIsInstance(Location(Location(MOCKS['location'])), Location, "Location model did not initiate properly")
+
+class TestLocationMethods(unittest.TestCase):
+    def setUp(self):
+        self.location = Location(MOCKS['location'])
+
+    def test_loan_updates(self):
+        self.location.id = "Zac"
+        self.assertEqual(self.location.id, "Zac", "ID did not correctly update")
+      
