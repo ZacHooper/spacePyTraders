0a119ae1c5ad6837aae9136688e81a201c764392
ZacHooper <zac.g.hooper@gmail.com>
2021-06-23 12:11:37 +1000

    Updated docs for 0.0.5 release

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Zac Hooper'
 author = 'Zac Hooper'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.5'
 
 
 # -- General configuration ---------------------------------------------------
